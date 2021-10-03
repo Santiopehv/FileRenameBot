7e453747f09ceb9e3ac767c99a8ce8fb1acc9a79
Harsha Sharma <87926976+Santiopehv@users.noreply.github.com>
2021-10-03 16:34:17 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -36,7 +36,7 @@ from PIL import Image
 from database.database import *
 from database.db import *
 
-@Mai_bOTs.on_message(pyrogram.filters.command(["scaption"]))
+@sctbots.on_message(pyrogram.filters.command(["scaption"]))
 async def set_caption(bot, update):
     if len(update.command) == 1:
         await update.reply_text(
